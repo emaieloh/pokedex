e91eadf7a4fe23331e1400643d0cf0cfe281f4ae
ganha <milo.somera@gmail.com>
2020-08-06 13:28:03 +0800

    Updates

---
+++ precache-manifest.99837077083546360407010b6585f047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa60b481c7a8226602f46cb1466d5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0a789ff565c42de653d",
+    "url": "/static/css/main.d84d99af.chunk.css"
+  },
+  {
+    "revision": "2e45c1f3dc17cb80cdc9",
+    "url": "/static/js/2.c10d64e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c10d64e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a789ff565c42de653d",
+    "url": "/static/js/main.1388defe.chunk.js"
+  },
+  {
+    "revision": "aebcdf6bcfd2acc05fbf",
+    "url": "/static/js/runtime-main.aebd6d6c.js"
+  }
+]);
